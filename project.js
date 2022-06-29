335c39531a88fbf82c67fded7e785811e26200ac
Rodrigo Leon <leonrrodrigo@hotmail.com>
2022-06-29 17:07:05 -0500

    Create playRound function

--- project.js
+++ project.js
@@ -4,4 +4,28 @@ function computerPlay() {
   return choice;
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "rock") return "TIE";
+  else if (playerSelection === "rock" && computerSelection === "scissors")
+    return "You win! rock beats scissors";
+  else if (playerSelection === "rock" && computerSelection === "paper")
+    return "You Lose! Paper beats rock";
+  else if (playerSelection === "paper" && computerSelection === "paper")
+    return "TIE";
+  else if (playerSelection === "paper" && computerSelection === "rock")
+    return "You win! Paper beats rock";
+  else if (playerSelection === "paper" && computerSelection === "scissors")
+    return "You Lose! Scissors beats Paper";
+  else if (playerSelection === "scissors" && computerSelection === "scissors")
+    return "TIE";
+  else if (playerSelection === "scissors" && computerSelection === "paper")
+    return "You Win! Scissors beats Paper";
+  else if (playerSelection === "scissors" && computerSelection === "rock")
+    return "You Lose! Rock beats Scissors";
+  else return "Error";
+}
+
+const playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
